348d3d13a0f958918dc2b7e3b0fd2b2ad2e7f23e
gr13nka <gran-san2003@yandex.ru>
2023-04-24 15:37:50 +0300

    Added Diamond

---
+++ Diamond/DiamondDraw.pas
@@ -0,0 +1,38 @@
+program Diamond;
+    writeln(cnt);
+var 
+    cnt:integer;
+
+procedure PrintChar(cnt:integer;ch:char);
+var 
+    i,j:integer;
+begin
+    for i:=1 to cnt do write(ch)
+end;
+
+begin
+
+    7
+    2n+1
+    2*3+1
+    
+    j=0
+    readln(cnt);
+    PrintChar(cnt div 2,' ');
+    PrinChar(1,'+');
+    PrintChar(cnt div 2,' ');
+    j++
+    j=1
+    PrintChar(cnt div 2 - j,' ');
+    PrinChar(1,'+');
+    PrinChar(j,' ');
+    PrinChar(1,'+');
+    PrinChar(cnt div 2-j,' ');
+
+    for j=0 to cnt do 
+        PrintChar(cnt div 2 - j, ' ');
+    
+
+    j = 1;
+    for i = 1 to  cnt div 2 - j do PrintChar
+end.
